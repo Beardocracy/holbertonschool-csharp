95b8a12fc384cc79caf7fcc306d84df70476daf4
Beardocracy <travisjbearden@gmail.com>
2020-07-14 17:06:12 -0500

    Added part 8

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        int digit = number % 10;
+        if (digit < 0)
+        {
+            digit *= -1;
+        }
+        Console.Write(digit.ToString("0"));
+        return digit;
+    }
+}
