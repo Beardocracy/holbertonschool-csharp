107f3f983c7c3a099eb81c75ecc5189386379603
Beardocracy <travisjbearden@gmail.com>
2020-07-14 17:30:22 -0500

    Added parrt 12

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _12_fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 101; i++)
+            {
+                string output = "";
+                if (i % 3 == 0)
+                {
+                    output += "Fizz";
+                }
+                if (i % 5 == 0)
+                {
+                    output += "Buzz";
+                }
+                if ((i % 3 != 0) && (i % 5 != 0))
+                {
+                    output += i.ToString();
+                }
+                if (i < 100)
+                {
+                    output += ", ";
+                }
+                Console.Write(output);
+            }
+            Console.WriteLine();
+        }
+    }
+}
