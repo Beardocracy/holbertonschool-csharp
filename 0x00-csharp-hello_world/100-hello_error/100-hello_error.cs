be8036356fe45108f5b6061b9c307a195b969231
Beardocracy <travisjbearden@gmail.com>
2020-07-13 21:31:34 -0500

    Added part 100

---
+++ 0x00-csharp-hello_world/100-hello_error/100-hello_error.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+using System.Numerics;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		TextWriter errorWriter = Console.Error;
+		errorWriter.WriteLine("and that piece of art is useful - Dora Korpar, 2015-10-19");
+		Environment.ExitCode = 1;
+	}
+}
