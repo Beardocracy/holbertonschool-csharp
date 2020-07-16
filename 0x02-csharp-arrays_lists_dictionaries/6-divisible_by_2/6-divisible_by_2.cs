1bc504b556fcfbb6494bd69ec67cc42898640e76
Beardocracy <travisjbearden@gmail.com>
2020-07-16 15:09:16 -0500

    Added part 6

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        if (myList.Count == 0)
+        {
+            Console.WriteLine("List is empty");
+            return null;
+        }
+        List<bool> boolList = new List<bool>();
+        foreach (int number in myList)
+        {
+            if (number % 2 == 0)
+            {
+                boolList.Add(true);
+            }
+            else
+            {
+                boolList.Add(false);
+            }
+        }
+        return boolList;
+    }
+}
