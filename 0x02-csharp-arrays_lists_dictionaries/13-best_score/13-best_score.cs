11d0ccc7f589c89b432217c5ea535daf94dd0e42
Beardocracy <travisjbearden@gmail.com>
2020-07-16 17:06:13 -0500

    Added part 13

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        List<int> valueList = myList.Values.ToList();
+        if (valueList.Count == 0)
+        {
+            return "None";
+        }
+        int max = 0;
+        string scorer = "";
+        foreach(KeyValuePair<string, int> kvp in myList)
+        {
+            if (kvp.Value > max)
+            {
+                max = kvp.Value;
+                scorer = kvp.Key;
+            }
+        }
+        return scorer;
+    }
+}
