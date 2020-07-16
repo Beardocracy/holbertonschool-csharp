f1e27e59366114427e9e747bb5b354da07e75d27
Beardocracy <travisjbearden@gmail.com>
2020-07-16 14:54:53 -0500

    Added check for negative size

--- 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -5,6 +5,11 @@ class List
 {
     public static List<int> CreatePrint(int size)
     {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
         List<int> newList = new List<int>();
         for (int i = 0; i < size; i++)
         {
