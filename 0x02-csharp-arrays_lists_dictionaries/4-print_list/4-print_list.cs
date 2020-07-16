63748f8247e83d68d10cbc9fe6808ec496c1988c
Beardocracy <travisjbearden@gmail.com>
2020-07-16 14:50:40 -0500

    Added part 4

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        List<int> newList = new List<int>();
+        for (int i = 0; i < size; i++)
+        {
+            newList.Add(i);
+            Console.Write(i);
+            if (i < size - 1)
+            {
+                Console.Write(" ");
+            }
+        }
+        Console.WriteLine();
+        return newList;
+    }
+}
