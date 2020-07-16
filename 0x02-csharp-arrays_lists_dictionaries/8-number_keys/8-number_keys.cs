ada8a4e67b6769be5f8803bbb7daabfdb5a38d7d
Beardocracy <travisjbearden@gmail.com>
2020-07-16 15:40:52 -0500

    Added part 8

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        Dictionary<string, string>.KeyCollection keyColl = myDict.Keys;
+        int count = 0;
+        foreach (string s in keyColl)
+        {
+            count++;
+        }
+        return count;
+    }
+}
