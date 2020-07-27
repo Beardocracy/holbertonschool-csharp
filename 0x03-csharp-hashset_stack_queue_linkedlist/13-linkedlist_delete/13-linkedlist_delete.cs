27e7ea47c2e5dc2fd604bd53bfd9cdf66ce08cfa
Beardocracy <travisjbearden@gmail.com>
2020-07-27 15:42:26 -0500

    Added part 13

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+   public static void Delete(LinkedList<int> myLList, int index)
+    {
+        if (index < 0 || index >= myLList.Count)
+        {
+            return;
+        }
+        if (index == 0)
+        {
+            myLList.RemoveFirst();
+            return;
+        }
+        if (index == myLList.Count - 1)
+        {
+            myLList.RemoveLast();
+            return;
+        }
+        LinkedListNode<int> current = myLList.First;
+        for (int i = 0; i < index; i++)
+        {
+            current = current.Next;
+        }
+        myLList.Remove(current);
+        return;
+
+    }
+}
