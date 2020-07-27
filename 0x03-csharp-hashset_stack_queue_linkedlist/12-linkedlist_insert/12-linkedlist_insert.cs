06a5e50e5a4b0040bcc96bc32d6c384f89f47531
Beardocracy <travisjbearden@gmail.com>
2020-07-27 15:26:39 -0500

    Added part 12

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+   public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+       LinkedListNode<int> newNode = new LinkedListNode<int>(n);
+       if (myLList.Count == 0)
+       {
+           myLList.AddFirst(newNode);
+           return newNode;
+       }
+       LinkedListNode<int> current = myLList.First;
+       if (current.Value > n)
+       {
+           myLList.AddFirst(newNode);
+           return newNode;
+       }
+       while (current != null)
+       {
+           if (current.Value <= n && (current.Next == null || current.Next.Value >= n))
+           {
+               myLList.AddAfter(current, newNode);
+               break;
+           }
+           current = current.Next;
+       }
+       return newNode;
+    }
+}
