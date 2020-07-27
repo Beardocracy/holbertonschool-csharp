f4a5525eb91136d3ce305c093a22f9aa10826ce7
Beardocracy <travisjbearden@gmail.com>
2020-07-27 12:01:53 -0500

    Added part 0

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        int sum = 0;
+        HashSet<int> uniqueSet = new HashSet<int>(myList);
+        foreach (int element in uniqueSet)
+        {
+            sum += element;
+        }
+        return sum;
+    }
+}
