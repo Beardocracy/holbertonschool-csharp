fde7f41f3d440bdb352fd71ed50b7b1da6076461
Beardocracy <travisjbearden@gmail.com>
2020-07-27 14:30:53 -0500

    Added part 8

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int index = -1;
+        int[] array = new int[myLList.Count];
+        myLList.CopyTo(array, 0);
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] == value)
+            {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+}
