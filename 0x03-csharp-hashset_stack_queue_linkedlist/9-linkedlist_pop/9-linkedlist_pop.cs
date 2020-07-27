e4be0bc8fdb3dd85ca561cc760b57d4f5da03763
Beardocracy <travisjbearden@gmail.com>
2020-07-27 14:38:44 -0500

    Added part 9

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        if (myLList.Count == 0)
+        {
+            return 0;
+        }
+        else
+        {
+            return myLList.First.Value;
+        }
+    }
+}
