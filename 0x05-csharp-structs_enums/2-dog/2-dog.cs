69384fad372566d377896bcd73b9b6faf1efe1ea
Beardocracy <travisjbearden@gmail.com>
2020-07-28 15:43:18 -0500

    Added part 2

---
+++ 0x05-csharp-structs_enums/2-dog/2-dog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+public struct Dog
+{
+    public Dog(string nameC, float ageC, string ownerC, Rating ratingC)
+    {
+        name = nameC;
+        age = ageC;
+        owner = ownerC;
+        rating = ratingC;
+    }
+
+    public string name { get; }
+    public float age { get; }
+    public string owner { get; }
+    public Rating rating { get; }
+}
