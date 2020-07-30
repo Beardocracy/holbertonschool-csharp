3ad48a26b24b413b5fdf62682ca99efc8be054e2
Beardocracy <travisjbearden@gmail.com>
2020-07-30 15:20:08 -0500

    Added part 1

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Enemies
+{
+
+/// <summary>
+/// Represents a Zombie
+/// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// Represents the health of the zombie
+        /// </summary>
+        public int health;
+        /// <summary>
+        /// Initializes a new instance of Zombie
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
