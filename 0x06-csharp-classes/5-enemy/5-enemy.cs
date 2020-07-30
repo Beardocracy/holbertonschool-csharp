1829e65d067f03e64c247b0b1115e44ec209e6b9
Beardocracy <travisjbearden@gmail.com>
2020-07-30 15:51:14 -0500

    Added part 5

---
+++ 0x06-csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Enemies
+{
+
+/// <summary>
+/// Represents a Zombie
+/// </summary>
+    public class Zombie
+    {
+        /* Represents the health */
+        private int health;
+        /* Represesnts the name */
+        private string name = "(No name)";
+
+        /// <summary>
+        /// Initializes a new instance of Zombie
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of Zombie
+        /// </summary>
+        /// <param name="value"></param>
+        public Zombie(int value)
+        {
+            if (value >= 0)
+            {
+                health = value;
+            }
+            else
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+        }
+        
+        /// <summary>
+        /// Returns the value of health
+        /// </summary>
+        /// <returns></returns>
+        public int GetHealth()
+        {
+            return health;
+        }
+
+        /// <summary>
+        /// Gets and sets the name.
+        /// </summary>
+        /// <value></value>
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        
+        /// <summary>
+        /// Overrides the tostring attribute.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return "Zombie name: " + name + " / Total Health: " + health;
+        }
+    }
+}
