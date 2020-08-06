3fabd594677d8e818e03a3774051022ae3566cf9
Beardocracy <travisjbearden@gmail.com>
2020-08-06 15:48:04 -0500

    Added part 1

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/// <summary>
+/// Contains IsInstanceOfArray method
+/// </summary>
+public class Obj
+{
+    /// <summary>
+    /// Returns true if the object is an instance of, or is an instance of a 
+    /// class which inherits from Array.
+    /// </summary>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        return (obj is Array);
+    }
+}
