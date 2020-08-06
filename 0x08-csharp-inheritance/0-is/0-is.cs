a569743efe8f71e7d78b8fb388c16c8c44832f3a
Beardocracy <travisjbearden@gmail.com>
2020-08-06 15:40:02 -0500

    Added part 0

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+/// <summary>
+/// Contains the method IsOfTypeInt
+/// </summary>
+public class Obj
+{
+    /// <summary>
+    /// Returns true if the obj passed is an int
+    /// </summary>
+    public static bool IsOfTypeInt(object obj)
+    {
+        return (obj.GetType() == typeof(int));
+    }
+}
