a4aa98cc521f84519073be8f376b0469add47f68
Beardocracy <travisjbearden@gmail.com>
2020-08-06 17:58:44 -0500

    Added part 7

---
+++ 0x08-csharp-inheritance/7-shape/7-shape.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+/// <summary>
+/// Represents a shape class
+/// </summary>
+class Shape
+{
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+/// <summary>
+/// Description of Rectangle class
+/// </summary>
+class Rectangle : Shape
+{
+    private int width;
+    private int height;
+
+    /// <summary>
+    /// Gets and sets width.
+    /// </summary>
+    public int Width
+    {
+        get { return width; }
+        set {
+            if (value < 0)
+            {
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            }
+            else
+            {
+                width = value;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Gets and sets height.
+    /// </summary>
+    /// <value></value>    
+    public int Height
+    {
+        get { return height; }
+        set {
+            if (value < 0)
+            {
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            }
+            else
+            {
+                height = value;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Returns the area of the rectangle.
+    /// </summary>
+    /// <returns></returns>
+    public new int Area()
+    {
+        return (height * width);
+    }
+
+    /// <summary>
+    /// Overrides the output for ToString(),
+    /// </summary>
+    public override string ToString()
+    {
+        return "[Rectangle] " + width + " / " + height;
+    }
+}
