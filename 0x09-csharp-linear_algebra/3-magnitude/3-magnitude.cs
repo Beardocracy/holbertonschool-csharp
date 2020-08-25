2aa4c211a4f3b0c2430eaa9863fb7010aa4878af
Beardocracy <travisjbearden@gmail.com>
2020-08-25 17:04:03 -0500

    Added part 3

---
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+/// <summary>
+/// This class contains VectorMath functions
+/// </summary>
+class VectorMath
+{
+    /// <summary>
+    /// Calculates and returns the length of a given vector.
+    /// </summary>
+    public static double Magnitude(double[] vector)
+    {
+        double mag;
+        double sumOfSquares = 0;
+    
+        if (vector.Length > 3 || vector.Length < 2)
+            return -1;
+        
+        for (int i = 0; i < vector.Length; i++)
+            sumOfSquares += vector[i] * vector[i];
+
+        mag = Math.Sqrt(sumOfSquares);
+        return Math.Round(mag, 2);
+    }
+}
