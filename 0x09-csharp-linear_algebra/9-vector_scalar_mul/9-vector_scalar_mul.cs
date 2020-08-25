ee893c639c64739280475568e262630cdc5b96bf
Beardocracy <travisjbearden@gmail.com>
2020-08-25 17:28:07 -0500

    Added part 9

---
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+/// <summary>
+/// This class contains VectorMath functions
+/// </summary>
+class VectorMath
+{
+    /// <summary>
+    ///  Multiplies a vector and a scalar and returns the resulting vector
+    /// </summary>
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        double[] result = new double[vector.Length];
+    
+        if (vector.Length > 3 || vector.Length < 2)
+            return new double[1] {-1};
+        
+        for (int i = 0; i < vector.Length; i++)
+            result[i] = vector[i] * scalar;
+
+        return result;
+    }
+}
