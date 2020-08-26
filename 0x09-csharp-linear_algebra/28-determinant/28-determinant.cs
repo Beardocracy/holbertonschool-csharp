3731dd68f467c9136798de6e891cd42c5b94e1aa
Beardocracy <travisjbearden@gmail.com>
2020-08-26 14:30:19 -0500

    Added rounding to 3x3:

--- 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -28,7 +28,7 @@ class MatrixMath
             result -= matrix[0,1] * (matrix[1, 0] * matrix[2, 2] - matrix[1, 2] * matrix[2, 0]);
             result += matrix[0,2] * (matrix[1, 0] * matrix[2, 1] - matrix[1, 1] * matrix[2, 0]);
 
-            return result;
+            return Math.Round(result, 2);
         }
         
         return -1;
