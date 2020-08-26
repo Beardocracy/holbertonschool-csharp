1a5c33895a6d5bc2567db27cd7e139fb3b8d8a82
Beardocracy <travisjbearden@gmail.com>
2020-08-26 16:57:02 -0500

    Fixed missing parentheses

--- 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -10,7 +10,7 @@ class MatrixMath
     /// </summary>
     public static double[,] Rotate2D(double[,] matrix, double angle)
     {
-        if (matrix.GetLength(0) != 2 || matrix.GetLength(10 != 2))
+        if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
             return new double[,] {{-1}};
         
         double [,] result = new double[2, 2];
